5e27a028529e6a034a6e7d3f71257278f5d26435
Frank Jimenez <jjcfrank@gmail.com>
2024-07-10 16:59:39 +0100

    Update v0.3.1

--- Formula/popgis.rb
+++ Formula/popgis.rb
@@ -3,7 +3,7 @@
 class Popgis < Formula
     desc "A blazing fast way to insert GeoJSON & ShapeFile into a PostGIS database"
     homepage "https://github.com/jjcfrancisco/popgis"
-    url "https://github.com/jjcfrancisco/popgis/archive/refs/tags/v0.3.0.tar.gz"
+    url "https://github.com/jjcfrancisco/popgis/archive/refs/tags/v0.3.1.tar.gz"
     sha256 "44b8a24fe1b8caba80951ada82acd1a6220d11c41be04bd9968a7c59f985539e"
   
     depends_on "rust" => :build
